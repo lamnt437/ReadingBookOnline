74db8ff382e6402ea42221b0c249e29683294b37
Lam Nguyen <lamnt437@gmail.com>
2019-08-26 22:47:53 +0700

    Sua DB lan cuoi v2

--- database/migrations/2019_08_22_031000_create_books_table.php
+++ database/migrations/2019_08_22_031000_create_books_table.php
@@ -20,9 +20,9 @@ class CreateBooksTable extends Migration
             $table->text('summary');
             $table->text('content');
             $table->string('language');
-            $table->integer('author_id')->unsigned();
+            // $table->integer('author_id')->unsigned();
             $table->integer('publisher_id')->unsigned();
-            $table->integer('category_id')->unsigned();
+            // $table->integer('category_id')->unsigned();
             $table->string('image');
             $table->timestamps();
             $table->foreign('publisher_id')->references('id')->on('publishers');
